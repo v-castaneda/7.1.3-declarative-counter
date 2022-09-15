141a4483755626cbe42d93772a40219041467eaa
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-15 08:45:15 -0400

    used declarative rendering to update page automatically

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import React, { useState } from 'react'
 
 function App() {
   let [ counter, changeCounter ] = useState(1)
+
+  /* making changeCounter a global variable for testing 
+  and debugging */
+  window.changeCounter = changeCounter
+
   return (
     <div className="App">
       <h1> {counter} </h1>
+      <h2> {counter} </h2>
+      <h3> {counter} </h3>
+      <h4> {counter} </h4>
     </div>
   );
 }
